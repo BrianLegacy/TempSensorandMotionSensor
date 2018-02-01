b197841caa6e53dc6dfa8b43c1b25d2cd70ea1f3
Josh Cardif <josh.cardif14@outlook.com>
2018-02-01 20:00:10 +0300

    Add project files.

---
+++ TempSensorandMotionSensor/TempSensorandMotionSensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TempSensorandMotionSensor.ino
+ Created:	2/1/2018 7:59:19 PM
+ Author:	Jcardif
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
